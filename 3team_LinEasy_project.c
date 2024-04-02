1301e45a95cfcba208702eb1f999a82a1512ec4c
dev0203vv <162665107+dev0203vv@users.noreply.github.com>
2024-04-02 21:11:09 +0900

    Add files via upload

--- 3team_LinEasy_project.c
+++ 3team_LinEasy_project.c
@@ -3,29 +3,42 @@
 #include <time.h>
 #include <string.h>
 
-#define NAMELEN 20
+#define NAMELEN 30
 #define NUM_MONSTERS 9 
-
- typedef struct monster {
-    char mon_name[NAMELEN];
-    char mon_T[NAMELEN];
-    int mon_HP;
-    int mon_dam;
-    int gold_drop;
-    int town_return_drop;
-}monster;
-
-
 int main() {
     srand(time(NULL));
-    int i;
-    int full_HP = 100;
-    int remain_HP = 100;
-    int user_dam = 100;
-    int floor = 1;
+    int full_HP = 100; // 확인용
+    int remain_HP = 100; // 확인용
+    int user_dam = 1000; // 확인용
+    int gold = 300;
+    int floor;
     int choice;
     int monster_dam;
     int monster_HP;
+    int town_return = 0;
+    int town_return_rand;
+    int boss_rand;
+    int real_boss_rand;
+    int final_real_boss_rand;
+    int gold_drop_rand;
+    int item_HP = 3; // 확인용
+    int potion; // 확인용
+    int run;
+    int class_rand = 0;
+    int class_num = 70;
+    int teleport = 0;
+    int teleport_num;
+    int teleport_rand = 0;
+    int equip_2T_rand = 0;
+    int equip_2T_num = 1;
+    int equip_2T =0;
+    int equip_3T_rand = 0;
+    int equip_3T_num = 1;
+    int equip_3T =0;
+    int equip_4T_rand = 0;
+    int equip_4T_num = 1;
+    int equip_4T =0;
+
     float HP_up;
     char monster_T[NAMELEN];
     char monster_name[NAMELEN];
@@ -38,85 +51,96 @@ int main() {
         "노주영", "김혜빈", "서훈", "오은지"
     };
 
-    monster monster[NUM_MONSTERS];
-
-    strcpy(monster[0].mon_name, "오크전사");
-    strcpy(monster[0].mon_T, "네임드");
-    monster[0].mon_HP = rand() % 100 + 50;
-    monster[0].mon_dam = rand() % 15 + 10;
-    monster[0].gold_drop = rand() % 10 + 5;
-
-    strcpy(monster[1].mon_name, "좀비");
-    strcpy(monster[1].mon_T, "네임드");
-    monster[1].mon_HP = rand() % 180 + 50;
-    monster[1].mon_dam = rand() % 30 + 17;
-    monster[1].gold_drop = rand() % 60 + 5;
-
-    strcpy(monster[2].mon_name, "구울");
-    strcpy(monster[2].mon_T, "네임드");
-    monster[2].mon_HP = rand()%280 + 120;
-    monster[2].mon_dam = rand()%45 + 20;
-    monster[2].gold_drop = rand()%100 + 5;
-
-    strcpy(monster[3].mon_name, "해골");
-    strcpy(monster[3].mon_T, "네임드");
-    monster[3].mon_HP = rand()%260 + 200;
-    monster[3].mon_dam = rand()%55 + 28;
-    monster[3].gold_drop = rand()%130 + 5;
-
-    strcpy(monster[4].mon_name, "스파토이");
-    strcpy(monster[4].mon_T, "네임드");
-    monster[4].mon_HP = rand()%360 + 260;
-    monster[4].mon_dam = rand()%75 + 32;
-    monster[4].gold_drop = rand()%200 + 5;
-    
-    strcpy(monster[5].mon_name, class_name[rand()%27]);
-    strcpy(monster[5].mon_T, "네임드");
-    monster[5].mon_HP = full_HP*2;
-    monster[5].mon_dam = rand()%300 + 100;
-    monster[5].gold_drop = rand()%500 + 5;
-
-    strcpy(monster[6].mon_name, "바포매트");
-    strcpy(monster[6].mon_T, "보스");
-    monster[6].mon_HP =  full_HP*5;
-    monster[6].mon_dam = rand()%450+180;
-    monster[6].gold_drop = rand()%700+5;
-
-    strcpy(monster[7].mon_name, "이동녘");
-    strcpy(monster[7].mon_T, "찐보스");
-    monster[7].mon_HP =  full_HP*7;
-    monster[7].mon_dam = rand()%550+300;
-    monster[7].gold_drop = rand()%1000+5;
-
-    strcpy(monster[8].mon_name , "류홍걸");
-    strcpy(monster[8].mon_T, "찐막보스");
-    monster[8].mon_HP =  full_HP*10;
-    monster[8].mon_dam = rand()%1300+500;
-    monster[8].gold_drop = rand()%3000+5;
-    
-
-    // for (i = 0; i < NUM_MONSTERS; i++) {
-    //     printf("%s %s\n체력: %d\n데미지: %d\n골드 획득량: %d\n\n", 
-    //         monster[i].mon_name, monster[i].mon_T, monster[i].mon_HP, monster[i].mon_dam, monster[i].gold_drop);
-    // }
-        while (1){
+    while (1){
+        
+        printf("층 수를 입력하세요: ");
+        scanf("%d",&floor);
         
             if (floor == 1){
                 strcpy(monster_name, "오크전사");
-                strcpy(monster_T, "네임드");
-                monster_dam = monster[0].mon_dam;
-                monster_HP = monster[0].mon_HP;
+                strcpy(monster_T, "노말 몬스터");
+                monster_dam = rand() % 15 + 10;
+                monster_HP = rand() % 100 + 50;
                 HP_up = 0.01;
-                floor++;
-                break;
+                gold_drop_rand = rand() % 10 + 5;
             }
-        }
-            printf("%s 몬스터 %s (이)가 나타났다!\n",monster_T,monster_name);
-            printf("몬스터 체력: %d\n",monster_HP);
+            else if (floor == 2 ){
+                strcpy(monster_name, "좀비");
+                strcpy(monster_T, "노말 몬스터");
+                monster_dam = rand() % 30 + 17;
+                monster_HP = rand() % 180 + 50;
+                HP_up = 0.02;
+                gold_drop_rand = rand() % 60 + 5;
+            }
+            else if (floor == 3 ){
+                strcpy(monster_name, "구울");
+                strcpy(monster_T, "노말 몬스터");
+                monster_dam = rand() % 40 + 25; 
+                monster_HP = rand()%280 + 120;
+                HP_up = 0.03;
+                gold_drop_rand = rand()%100 + 5;
+            }
+            else if (floor == 4 ){
+                strcpy(monster_name, "해골");
+                strcpy(monster_T, "노말 몬스터");
+                monster_dam = rand()%55 + 28;
+                monster_HP = rand()%260 + 200;
+                HP_up = 0.05;
+                gold_drop_rand = rand()%130 + 5;
+            }
+            else if (floor == 5 ){
+                strcpy(monster_name, "스파토이");
+                strcpy(monster_T, "노말 몬스터");
+                monster_dam = rand()%75 + 32;
+                monster_HP = rand()%360 + 260;
+                HP_up = 0.07;
+                gold_drop_rand = rand()%200 + 5;
+            }
+
+            boss_rand = rand()%100;
+            if (floor == 5 && boss_rand >= 80){
+                strcpy(monster_name, "바포매트");
+                strcpy(monster_T, "보스 몬스터");
+                monster_dam = rand()%300 + 100;
+                monster_HP = remain_HP*5;
+                HP_up = 0.3;
+                gold_drop_rand = rand()%700+5;
+            }
+            real_boss_rand = rand()%100;
+            if (floor == 5 && real_boss_rand >= 90){
+                strcpy(monster_name, "이동녘");
+                strcpy(monster_T, "찐보스 몬스터");
+                monster_dam = rand()%550+300;
+                monster_HP = remain_HP*7;
+                HP_up = 0.6;
+                gold_drop_rand = rand()%1000+5;
+            }
+            final_real_boss_rand = rand()%100;
+            if (floor == 5 && final_real_boss_rand >= 95){
+                strcpy(monster_name, "류홍걸");
+                strcpy(monster_T, "찐막보스 몬스터");
+                monster_dam = rand()%1300+500;
+                monster_HP = remain_HP*10;
+                HP_up = 1.0;
+                gold_drop_rand = rand()%3000+5;
+            }
+            class_rand = rand()%100;
+            if (class_rand >= class_num){
+                strcpy(monster_name, class_name[rand()%27]);
+                strcpy(monster_T, "네임드 용사");
+                monster_dam = rand()%300 + 100;
+                monster_HP = remain_HP*2;
+                HP_up = 0.2;
+                gold_drop_rand = rand()%500 + 5;
+            }
+        
+            printf("\n%s %s 등장!\n",monster_T,monster_name);
+            printf("%s 체력: %d\n",monster_name,monster_HP);
+            printf("전투가 시작됩니다\n");
+
         while(1){
             int flag = 0;
             int ichoice =0;
-            int shieldactivated=0;
            
             printf("나의 체력 : %d\n",remain_HP);
             printf("\n1.공격  2.아이템  3.도망\n 원하는 번호를 선택하시오. : ");
@@ -125,86 +149,196 @@ int main() {
                 switch(choice)
                 {
                     case 1:
-                        printf("현재 공격력 : %d\n",user_dam);
-                       
-                        printf("용사복이가 몬스터에게 공격을 가합니다.\n");
-            
-                   
-                        printf("몬스터 남은 체력 : %d\n",monster_HP);
-                       
-                        printf("나의 남은체력 : %d\n",remain_HP);
-                        
-                        if (monster[0].mon_HP <= 0)
+                            
+                            
+                            printf("\n\n용사복이가 %s에게 공격을 가합니다.\n",monster_name);
+                             printf("%d의 데미지!\n", user_dam);
+                            monster_HP -= user_dam;
+                            printf("%s 남은 체력 : %d\n",monster_name,monster_HP);
+                            
+                        if (monster_HP <= 0)
                         {   
                            
-                            printf("%s(을)를 처치했습니다!\n",monster[0].mon_name);
+                            printf("%s(을)를 처치했습니다!\n",monster_name);
                             full_HP = full_HP + (full_HP * HP_up);
-                           
-                            printf("전투에서 승리하여 체력량이 증가합니다. : %d\n",full_HP);
-                                    
-                          
+                            gold += gold_drop_rand;
+                            printf("%d G 획득 했습니다. 현재 소지 골드: %d G\n",gold_drop_rand,gold);
+                            printf("\n전투에서 승리하여 체력량이 증가합니다. : %d\n",full_HP);
 
+                            town_return_rand = rand()%100;
+                            if(town_return_rand >= 80)
+                            {
+                                town_return += 1;
+                               
+                                printf("\n마을귀환 주문서를 1개 획득 했습니다.\n");
+                                printf("현재 보유한 마을귀환 주문서의 개수는 %d개 입니다.\n", town_return);
+                            }
+                            if(teleport_rand >= 70)
+                            {
+                                teleport_num = rand()%3+1;
+                                teleport += teleport_num;
+                                teleport_rand = 0;
+                               
+                                printf("\n순간이동 주문서를 %d개 획득 했습니다.\n",teleport_num);
+                                printf("현재 보유한 순간이동 주문서의 개수는 %d개 입니다.\n", teleport);
+                            }
+                            if(equip_2T_rand >= equip_2T_num){
+                                equip_2T += 1;
+                                equip_2T_rand = 0;
+
+                                printf("\n2티어 장비를 획득 했습니다.\n");
+                            }
+                            if(equip_3T_rand >= equip_3T_num){
+                                equip_3T += 1;
+                                equip_3T_rand = 0;
+
+                                printf("\n3티어 장비를 획득 했습니다.\n");
+                            }
+                            if(equip_4T_rand >= equip_4T_num){
+                                equip_4T += 1;
+                                equip_4T_rand = 0;
+                                printf("\n4티어 장비를 획득 했습니다.\n");
+                            }
+                            if(strcmp(monster_T,"네임드") == 0){
+                                class_num = 70;
+                                teleport_rand = rand()%100;
+                                equip_2T_num = 80;
+                                equip_3T_num = 80;
+                                equip_2T_rand = rand()%100;
+                                equip_3T_rand = rand()%100;
+                            }
+                            else if(strcmp(monster_T,"보스 몬스터") == 0){
+                                class_num = 80;
+                                teleport_rand = rand()%100;
+                                equip_3T_num = 80;
+                                equip_4T_num = 95;
+                                equip_3T_rand = rand()%100;
+                                equip_4T_rand = rand()%100;
+                            }
+                            else if(strcmp(monster_T,"찐보스 몬스터") == 0){
+                                class_num = 90;
+                                teleport_rand = rand()%100;
+                                equip_3T_num = 80;
+                                equip_4T_num = 90;
+                                equip_3T_rand = rand()%100;
+                                equip_4T_rand = rand()%100;
+                            }
+                            else if(strcmp(monster_T,"찐막보스 몬스터") == 0){
+                                class_num = 95;
+                                teleport_rand = rand()%100;
+                                equip_3T_num = 70;
+                                equip_4T_num = 80;
+                                equip_3T_rand = rand()%100;
+                                equip_4T_rand = rand()%100;
+                            }
+                            else if (strcmp(monster_name,"해골") == 0 || strcmp(monster_name,"스파토이") == 0 )
+                            {
+                                equip_2T_num = 80;
+                                equip_2T_rand = rand()%100;
+                            }
                             
-                                   
-                            printf("체력이 회복되었습니다. 현재 체력은 %d입니다.\n", remain_HP);
-                            if(remain_HP >= full_HP)
+                            flag = 1;
+                        }
+                        break;
+                    case 2:
+                        
+                            printf("사용할 아이템을 선택해 주세요.\n");
+                            printf("1. 빨간물약(%d)   2. 주황물약(%d)   3. 맑은물약(%d)   4. 고농도물약(%d)   5.취소\n",item_HP,item_HP,item_HP,item_HP);
+                            scanf("%d", &ichoice);
+                            if (ichoice == 1)
+                                potion = 30;
+                            else if (ichoice == 2)
+                                potion = 50;
+                            else if (ichoice == 3)
+                                potion = 70;
+                            else if (ichoice == 4)
+                                potion = 150;
+                            else if (ichoice == 5)
+                                break;
+                            else {
+                                printf("번호를 다시 입력해주세요.\n");
+                                continue;
+                            }
+                            if(item_HP == 0)
                             {   
                                 
-                                printf("남아있는 체력은 최대체력을 넘을 수 없습니다. 현재 체력 : %d\n", remain_HP);
-                                remain_HP = full_HP;
+                                printf("소지한 물약이 없습니다.\n");
+                                break;
                             }
-
-                            if(shiledrandom <= 5)
-                            {   item_shield += 1;
-                               
-                                printf("절대무적방패를 획득하였습니다!\n");
-                               
-                                printf("현재 소지 방패 수는 %d개입니다.\n", item_shield);
-                            }
-
-                            if(item_shield >= 10)
-                            {
-                                item_shield = 10;
-                               
-                                printf("절대무적방패가 소지한도를 넘어서 일부 사라집니다.\n");
-                                        
-                            }
-
-                            drugrandom = rand()%100;
-                            if(drugrandom >= 50)
-                            {
-                                item_drug += 1;
-                               
-                                printf("환약을 획득 했습니다.\n");
-                                if(item_drug >= 10)
-                                {
-                                    item_drug = 10;
-                                   
-                                    printf("환약의 소지 한도가 초과 되었습니다.\n");
+                            else
+                            {   
+                                
+                                printf("현재 체력 : %d  \n",remain_HP);
+                                remain_HP = remain_HP + potion;
+                                
+                                printf("체력이 %d 회복되었습니다.\n",potion);
+                                if(remain_HP >full_HP)
+                                {   
+                                    
+                                    printf("\n회복체력이 최대 체력 %d를 초과합니다.\n",full_HP);
+                                    
+                                    printf("체력이 최대 체력만큼 재조정 됩니다.\n");
+                                    remain_HP = full_HP;
                                 }
-                                else
-                                   
-                                    printf("현재 보유한 환약 수는..%d개 입니다.\n", item_drug);
+                                
+                                printf("회복 후 체력 : %d  \n",remain_HP);
+                                item_HP -= 1;
+                                
+                                printf("남아있는 포션 : %d\n",item_HP);
+                                break;
                             }
-                            noterand = rand()%100;
-                            if(noterand >= 80)
-                            {
-                                item_note += 1;
-                               
-                                printf("데스노트를 획득 했습니다.\n");
-                                if(item_note >= 3)
-                                {
-                                    item_note = 3;
-                                   
-                                    printf("데스노트의 소지 한도가 초과 되었습니다.\n");
-                                }
-                                else
-                                    printf("현재 보유한 데스노트 수는 %d개 입니다.\n", item_note);
+                            
+                            break;
+                            
+                    case 3:
+                            run = rand()%10+1;
+                            if (run >= 8)
+                            {   
+                                printf("\n%s에게서 도망쳤습니다.\n",monster_name);
+                                flag=1;
                             }
-                            flag = 1;
-        
-                        }    
+                            else
+                            {   
+                                printf("\n도망에 실패했습니다.\n%s의 턴으로 전환됩니다.\n",monster_name);
+                            }
+                            
+                            break;
+                            
+                    default:
+                            
+                            printf("번호를 다시 입력해주세요.\n");
+                            continue;
+                    }    
+                    
+
+                if(flag == 1)
+                {
+                    
+                    break; //맵 이동으로 전환
                 }
+        /////////////////////////////////////////////////////////////몬스터 턴////////////////////////////////////////////////////////////////////////
+                
+                printf("\n%s(이)가 용사복이를 공격합니다!\n",monster_name);
+
+                printf("%d의 데미지!\n", monster_dam);
+                remain_HP = remain_HP - monster_dam;
+                
+                // printf("나의 남은 체력 : %d\n",remain_HP);
+
+                if(remain_HP < 0)
+                {   
+                    
+                    printf("\n플레이어가 사망했습니다.\n");
+                    remain_HP = full_HP*0.1;
+                    printf("마을에서 부활합니다. 체력이 10%% 회복됩니다.\n");
+                    printf("현재체력: %d\n",remain_HP);
+                    break;
+                }
+                continue;
+               
+        }
+
+    
     }
     return 0;
 }
